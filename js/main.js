2a7f4d738edf9df8d3219dd935ce300d74615642
Pablo Peña <146494145+KingVizard@users.noreply.github.com>
2024-02-09 21:02:20 -0600

    pruebas de js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function registrar() {
+    console.log("diste un click");
+}
+
+
+
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// const auth = getAuth();
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
